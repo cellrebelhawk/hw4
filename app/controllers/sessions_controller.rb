a866d6580f0af24a1f46f775be49e527f12c6eba
Marko Jelasity <marko.jelasity@kellogg.northwestern.edu>
2025-03-06 23:49:45 +0000

    Created basic sign-up logic, inlcuding placing cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Find user
+    @user = User.find_by({"email" => params["email"]})
+    # If user exists, check if they know password
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome!"
+        redirect_to "/users/#{@user["id"]}"
+      else
+      flash["notice"] = "Your username or password are incorrect or do not exist"
+        redirect_to "/sessions/new"
+      end
+    # 4. if the user doesn't exist or they don't know their password -> login fails
+    else
+      flash["notice"] = "Your username or password are incorrect or do not exist"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    # logout the user
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+
   def new
   end
 
   def create
+    if User.find_by({"email" => params["email"].downcase})
+      redirect_to "/sessions/new"
+      flash["notice"] = "User already exists, log in here!"
+  else
     @user = User.new
     @user["first_name"] = params["first_name"]
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
-    redirect_to "/"
+    session["user_id"] = @user["id"]
+    redirect_to "/users/#{@user["id"]}"
+  end
   end
 end
